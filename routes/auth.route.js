67f34bde3a58aaa8ed829c956e3763debf79eab3
Gaurav <gauravcodes3512@gmail.com>
2025-06-21 23:31:45 +0530

    chnage 1

--- routes/auth.route.js
+++ routes/auth.route.js
@@ -5,6 +5,7 @@ const authRouter = Router();
 authRouter.post('/sign-up',(req,res)=>res.send({mesasge:'Sign-up'}))
 authRouter.post('/sign-in',(req,res)=>res.send({mesasge:'Sign-in'}))
 authRouter.post('/sign-out',(req,res)=>res.send({mesasge:'Sign-out'}))
+authRouter.post('/sign-out',(req,res)=>res.send({mesasge:'Sign-out'}))
 
 
 export default authRouter;
