f3324435fbb06c577345603e49f72324dd1a4708
Gaurav <gauravcodes3512@gmail.com>
2025-06-23 12:08:12 +0530

    new changes

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -25,3 +25,19 @@ export const getUser = async (req, res, next) => {
     next(error);
   }
 }
+
+export const deleteUser=async (req,res,next)=>{
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (!user) {
+      const error = new Error('User not found');
+      error.statusCode = 404;
+      throw error;
+    }
+
+    res.status(200).json({ success: true, data: user });
+  } catch (error) {
+    next(error);
+  }
+}
--- routes/user.router.js
+++ routes/user.router.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import authorise from '../middlewares/auth.middleware.js';
 
 // import authorize from '../middlewares/auth.middleware.js'
-import { getUser, getUsers } from '../controllers/user.controller.js'
+import { getUser, getUsers ,deleteUser} from '../controllers/user.controller.js'
 
 const userRouter = Router();
 
@@ -10,10 +10,10 @@ userRouter.get('/', getUsers);
 
 userRouter.get('/:id',authorise, getUser);
 
-userRouter.post('/', (req, res) => res.send({ title: 'CREATE new user' }));
+// userRouter.post('/', (req, res) => res.send({ title: 'CREATE new user' }));
 
 userRouter.put('/:id', (req, res) => res.send({ title: 'UPDATE user' }));
 
-userRouter.delete('/:id', (req, res) => res.send({ title: 'DELETE user' }));
+userRouter.delete('/:id', authorise,deleteUser)
 
 export default userRouter;
